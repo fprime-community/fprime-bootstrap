faf3684e4a62075d16398895911c86fe7a427119
chammard <thomas.boyer.chammard@jpl.nasa.gov>
2024-01-29 17:26:43 -0800

    Initial setup of fprime_bootstrap

---
+++ src/fprime_bootstrap/__main__.py
@@ -0,0 +1,52 @@
+"""
+fprime_bootstrap.__main__:
+
+Main entry point for fprime-bootstrap
+
+@author thomas-bc
+"""
+
+import sys
+import os
+
+import logging
+import argparse
+
+LOGGER = logging.getLogger("fprime_bootstrap")
+
+
+def main():
+    """Run wrapper, to point a console_script at"""
+
+    parser = argparse.ArgumentParser(description="F Prime bootstrapping tool")
+    subparsers = parser.add_subparsers(title="subcommands", dest="command")
+    project_parser = subparsers.add_parser('project', help='Create a new F´ project')
+    project_parser.add_argument(
+        "--path",
+        type=str,
+        help="Path to create the project in (default: current directory)",
+        default=os.getcwd(),
+    )
+    project_parser.add_argument(
+        "--no-venv",
+        action="store_true",
+        help="Do not create a virtual environment in the project",
+        default=False,
+    )
+
+    args = parser.parse_args()
+
+    if args.command == "project":
+        from fprime_bootstrap import bootstrap_project
+        return bootstrap_project.bootstrap_project(args)
+
+
+    LOGGER.error("[ERROR] No sub-command supplied")
+    parser.print_help()
+    sys.exit(1)
+
+
+
+
+if __name__ == "__main__":
+    sys.exit(main())
---
+++ src/fprime_bootstrap/bootstrap_project.py
@@ -0,0 +1,55 @@
+"""
+fprime_bootstrap.bootstrap_project:
+
+Bootstraps a new project using cookiecuter
+
+@author thomas-bc
+"""
+
+import sys
+from pathlib import Path
+import shutil
+from cookiecutter.main import cookiecutter
+
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    import argparse
+
+
+def bootstrap_project(parsed_args: "argparse.Namespace"):
+    """Creates a new F' project"""
+
+    # Check if Git is installed and available - needed for cloning F' as submodule
+    if not shutil.which("git"):
+        print(
+            "[ERROR] Git is not installed or in PATH. Please install Git and try again.",
+            file=sys.stderr,
+        )
+        return 1
+
+    source = (
+        Path(__file__).parent / "cookiecutter_templates/cookiecutter-fprime-project"
+    )
+    try:
+        cookiecutter(
+            str(source),
+            output_dir=parsed_args.path,
+            extra_context={
+                "__install_venv": "no" if parsed_args.no_venv else "yes",
+                "__git_init": "yes" if parsed_args.no_git_init else "no",
+            },
+        )
+    except PermissionError as out_directory_error:
+        print(
+            f"{out_directory_error}. Use --overwrite to overwrite (will not delete non-generated files).",
+            file=sys.stderr,
+        )
+        return 1
+    except FileNotFoundError as e:
+        print(
+            f"{e}. Permission denied to write to the directory.",
+            file=sys.stderr,
+        )
+        return 1
+    return 0
