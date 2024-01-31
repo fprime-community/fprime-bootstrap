bdb428540ff191504e614503f763a5379c629d93
chammard <thomas.boyer.chammard@jpl.nasa.gov>
2024-01-30 17:22:39 -0800

    Clean pre_gen check

--- src/fprime_bootstrap/cookiecutter_templates/cookiecutter-fprime-project/hooks/pre_gen_project.py
+++ src/fprime_bootstrap/cookiecutter_templates/cookiecutter-fprime-project/hooks/pre_gen_project.py
@@ -24,7 +24,9 @@ invalid_characters = [
         "|",
         "=",
 ]
+
 for char in "{{ cookiecutter.project_name }}":
     if char in invalid_characters:
-        print("[ERROR]: Invalid character in project name: {}".format(char))
+        print("[ERROR] Invalid character in project name: {}".format(char))
+        print("[ERROR] Unacceptable project name. Do not use spaces or special characters")
         sys.exit(1)
