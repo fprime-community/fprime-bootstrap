4da6e292e593ccfceb9deecfa3dee4a54eca6507
chammard <thomas.boyer.chammard@jpl.nasa.gov>
2024-02-20 18:57:25 -0800

    Initial MVP

--- src/fprime_bootstrap/bootstrap_project.py
+++ src/fprime_bootstrap/bootstrap_project.py
@@ -6,10 +6,12 @@ Bootstraps a new project using cookiecuter
 @author thomas-bc
 """
 
-import sys
-from pathlib import Path
+import json
 import shutil
-from cookiecutter.main import cookiecutter
+import subprocess
+import sys
+from urllib.request import urlopen
+from pathlib import Path
 
 from typing import TYPE_CHECKING
 
@@ -17,8 +19,15 @@ if TYPE_CHECKING:
     import argparse
 
 
+DEFAULT_PROJECT_NAME = "MyProject"
+
+
 def bootstrap_project(parsed_args: "argparse.Namespace"):
     """Creates a new F' project"""
+
+    # Check Python version
+    if sys.version_info < (3, 8):
+        print("[WARNING] Python 3.8 or higher is required to use the F´ Python tooling suite.")
 
     # Check if Git is installed and available - needed for cloning F' as submodule
     if not shutil.which("git"):
@@ -28,17 +37,24 @@ def bootstrap_project(parsed_args: "argparse.Namespace"):
         )
         return 1
 
-    source = (
-        Path(__file__).parent / "cookiecutter_templates/cookiecutter-fprime-project"
-    )
+    target_dir = Path(parsed_args.path)
+    # Ask user for project name
+    project_name = input(f"Project name ({DEFAULT_PROJECT_NAME}): ")
+    if not is_valid_name(project_name):
+        sys.exit(1)
+    elif not project_name:
+        project_name = DEFAULT_PROJECT_NAME
+
+    project_path = target_dir / project_name
+
     try:
-        cookiecutter(
-            str(source),
-            output_dir=parsed_args.path,
-            extra_context={
-                "__install_venv": "no" if parsed_args.no_venv else "yes",
-            },
-        )
+        generate_boilerplate_project(project_path, project_name)
+        setup_git_repo(project_path)
+        if not parsed_args.no_venv:
+            setup_venv(project_path)
+
+        print_success_message(project_name)
+
     except PermissionError as out_directory_error:
         print(
             f"{out_directory_error}. Use --overwrite to overwrite (will not delete non-generated files).",
@@ -52,3 +68,166 @@ def bootstrap_project(parsed_args: "argparse.Namespace"):
         )
         return 1
     return 0
+
+
+def is_valid_name(project_name: str) -> bool:
+    """Checks if a project name is valid"""
+    invalid_characters = [
+        "#",
+        "%",
+        "&",
+        "{",
+        "}",
+        "/",
+        "\\",
+        "<",
+        ">",
+        "*",
+        "?",
+        " ",
+        "$",
+        "!",
+        "'",
+        '"',
+        ":",
+        "@",
+        "+",
+        "`",
+        "|",
+        "=",
+    ]
+    for char in project_name:
+        if char in invalid_characters:
+            print("[ERROR] Invalid character in project name: {}".format(char))
+            print(
+                "[ERROR] Invalid project name. Do not use spaces or special characters"
+            )
+            return False
+    return True
+
+
+def setup_git_repo(project_path: Path):
+    """Sets up a new git project"""
+    # Retrieve latest F' release
+    with urlopen("https://api.github.com/repos/nasa/fprime/releases/latest") as url:
+        fprime_latest_release = json.loads(url.read().decode())
+        latest_tag_name = fprime_latest_release["tag_name"]
+
+    # Initialize git repository
+    subprocess.run(["git", "init"], cwd=project_path)
+
+    # Add F' as a submodule
+    print(f"[INFO] Checking out F' submodule at latest release: {latest_tag_name}")
+    subprocess.run(
+        [
+            "git",
+            "submodule",
+            "add",
+            "--depth",
+            "1",
+            "https://github.com/nasa/fprime.git",
+        ],
+        cwd=project_path,
+    )
+    # Checkout F´ submodules (e.g. googletest)
+    res = subprocess.run(
+        ["git", "submodule", "update", "--init", "--recursive"],
+        capture_output=True,
+        cwd=project_path,
+    )
+    if res.returncode != 0:
+        print(
+            "[WARNING] Unable to initialize submodules. Functionality may be limited."
+        )
+
+    fprime_path = project_path / "fprime"
+
+    subprocess.run(
+        ["git", "fetch", "origin", "--depth", "1", "tag", latest_tag_name],
+        cwd=fprime_path,
+        capture_output=True,
+    )
+
+    # Checkout requested branch/tag
+    res = subprocess.run(
+        ["git", "checkout", latest_tag_name],
+        cwd=fprime_path,
+        capture_output=True,
+    )
+    if res.returncode != 0:
+        print(f"[ERROR] Unable to checkout tag: {latest_tag_name}. Exit...")
+        sys.exit(1)
+
+
+def setup_venv(project_path: Path):
+    """Sets up a new virtual environment"""
+    venv_path = project_path / "fprime-venv"
+
+    print(f"[INFO] Creating virtual environment in {venv_path} ...")
+    subprocess.run([Path(sys.executable), "-m", "venv", venv_path])
+
+    # Find pip
+    pip = None
+    if (venv_path / "bin/pip").exists():
+        pip = venv_path / "bin/pip"
+    elif (venv_path / "bin/pip3").exists():
+        pip = venv_path / "bin/pip3"
+    else:
+        raise FileNotFoundError("Could not find pip executable in venv.")
+
+    # Upgrade pip
+    print("[INFO] Upgrading pip...")
+    subprocess.run([pip, "install", "--upgrade", "pip"])
+    # Install requirements.txt
+    print("[INFO] Installing F´ dependencies...")
+    subprocess.run(
+        [
+            pip,
+            "install",
+            "-Ur",
+            project_path / "fprime" / "requirements.txt",
+        ]
+    )
+
+
+def generate_boilerplate_project(project_path: Path, project_name: str):
+    """Generates a new project"""
+    source = Path(__file__).parent / "templates/fprime-project-template"
+    # copy files from template into target path
+    shutil.copytree(source, project_path)
+
+    # Iterate over all files in path and replace {{FPRIME_PROJECT_NAME}} with project_name
+    for file in project_path.rglob("*"):
+        if file.is_file():
+            with file.open("r") as f:
+                contents = f.read()
+            with file.open("w") as f:
+                f.write(contents.replace(r"{{FPRIME_PROJECT_NAME}}", project_name))
+
+
+def print_success_message(project_name: str):
+    """Prints a success message"""
+    print(
+        f"""
+################################################################
+
+Congratulations! You have successfully created a new F´ project.
+
+A git repository has been initialized and F´ has been added as a
+submodule, you can now create your first commit.
+
+Get started with your F´ project:
+
+-- Remember to always activate the virtual environment --
+cd {project_name}
+. fprime-venv/bin/activate
+
+-- Create a new component --
+fprime-util new --component
+
+-- Create a new deployment --
+fprime-util new --deployment
+
+################################################################
+"""
+    )
