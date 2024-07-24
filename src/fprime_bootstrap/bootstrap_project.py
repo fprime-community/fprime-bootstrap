4b251e98c270fd7dff474f4d1cf891a2db6478f1
M Starch <LeStarch@googlemail.com>
2024-07-24 13:12:04 -0700

    Adds CMakePreset.json to new projects (#7)
    * Adding CMakePresets.json to project template

--- src/fprime_bootstrap/bootstrap_project.py
+++ src/fprime_bootstrap/bootstrap_project.py
@@ -203,9 +203,7 @@ def get_latest_fprime_release() -> str:
     but it's the best we can do without the API.
     """
     try:
-        with urlopen(
-            "https://api.github.com/repos/nasa/fprime/releases/latestee"
-        ) as url:
+        with urlopen("https://api.github.com/repos/nasa/fprime/releases/latest") as url:
             fprime_latest_release = json.loads(url.read().decode())
             return fprime_latest_release["tag_name"]
     except HTTPError:
