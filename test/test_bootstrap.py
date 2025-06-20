691a0cda08a44ddcd68ce403dcd2a38fd225530f
Remzell Maranan <marananremzell@gmail.com>
2025-06-20 10:26:08 +0800

    move replace to a variable

--- test/test_bootstrap.py
+++ test/test_bootstrap.py
@@ -88,8 +88,9 @@ def test_fprime_project_name_replace():
 
                 for index in range(len(contents)):
                     if contents[index].find(r"{{FPRIME_PROJECT_NAME}}") != -1:
+                        clean_string = contents[index].replace('\n', '')
                         lines.append(
-                            f"\tLine {index + 1}: {contents[index].replace('\n', '')}"
+                            f"\tLine {index + 1}: {clean_string}"
                         )
 
                 if lines:
